ef0e16738e642827d5e9798ea1b2b3c9a8b5f386
Padma Patil <padma@email.com>
2023-11-02 13:03:52 -0700

    Add js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    let form = document.getElementById("Character");
+    form.onsubmit = function (text) {
+        const charName = document.getElementById("charName").value;
+        const className = document.getElementById("charclass").value;
+        const alingment = document.getElementById("charalingment").value;
+
+
+
+        document.querySelector("span#person").innerText = charName;
+        document.querySelector("span#person1").innerText = className;
+        document.querySelector("span#person2").innerText = alingment;
+
+        document.querySelector("div#charResult").removeAttribute("class");
+
+        event.preventDefault();
+    };
+};
